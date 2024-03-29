fd2be67063374e8a4be30fd5f01fbe282ad019f6
Guiza4 <youssef.guiza@yahoo.com>
2024-03-29 10:20:48 +0100

    Sql to PostgreSEKULE

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,42 +1,47 @@
-CREATE TABLE Utilisateur (
-                             ID_Utilisateur INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                             Nom VARCHAR(255),
-                             Prenom VARCHAR(255),
-                             Type_User ENUM ('Admin','Client')
+CREATE TABLE Utilisateur
+(
+    ID_Utilisateur SERIAL PRIMARY KEY,
+    Nom            VARCHAR(255),
+    Prenom         VARCHAR(255),
+    Type_User      VARCHAR(255) CHECK (Type_User IN ('Admin', 'Client'))
 );
 
-CREATE TABLE NoteFilm (
-                          ID_Utilisateur INT,
-                          ID_Film INT,
-                          Note INT CHECK ( Note>=0 AND Note<=5 ),
-                          Commentaire VARCHAR(255),
-                          PRIMARY KEY (ID_Utilisateur, ID_Film),
-                          FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateur(ID_Utilisateur),
-                          FOREIGN KEY (ID_Film) REFERENCES Film(ID_Film)
+CREATE TABLE Film
+(
+    ID_Film  SERIAL PRIMARY KEY,
+    Nom_Film VARCHAR(255),
+    Genre    VARCHAR(255) CHECK (Genre IN ('Action', 'Comédie', 'Horreur', 'Drame', 'Romance')),
+    Langue   VARCHAR(255) CHECK (Langue IN ('Français', 'Anglais')),
+    Synopsis TEXT
 );
 
-CREATE TABLE NoteLieu (
-                          ID_Utilisateur INT,
-                          ID_Lieu INT,
-                          Note INT CHECK ( Note>=0 AND Note<=5 ),
-                          Commentaire VARCHAR(255),
-                          PRIMARY KEY (ID_Utilisateur, ID_Lieu),
-                          FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateur(ID_Utilisateur),
-                          FOREIGN KEY (ID_Lieu) REFERENCES Lieu(ID_Lieu)
+CREATE TABLE Lieu
+(
+    ID_Lieu     SERIAL PRIMARY KEY,
+    Adresse     VARCHAR(255),
+    Ville       VARCHAR(255),
+    Type_Lieu   VARCHAR(255) CHECK (Type_Lieu IN ('Restaurant', 'Bar', 'Cinema', 'Parc', 'Musée')),
+    Description TEXT
 );
 
-CREATE TABLE Film (
-                      ID_Film INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                      Nom_Film VARCHAR(255),
-                      Genre ENUM ('Action','Comédie','Horreur','Drame','Romance'),
-                      Langue ENUM ('Français', 'Anglais'),
-                      Synopsis VARCHAR(255)
+CREATE TABLE NoteFilm
+(
+    ID_Utilisateur INT,
+    ID_Film        INT,
+    Note           INT CHECK (Note >= 0 AND Note <= 5),
+    Commentaire    VARCHAR(255),
+    PRIMARY KEY (ID_Utilisateur, ID_Film),
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateur (ID_Utilisateur),
+    FOREIGN KEY (ID_Film) REFERENCES Film (ID_Film)
 );
 
-CREATE TABLE Lieu (
-                       ID_Lieu INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                       Adresse VARCHAR(255),
-                       Ville VARCHAR(255),
-                       Type_Lieu ENUM('Restaurant','Bar','Cinema','Parc','Musée'),
-                       Description VARCHAR(255)
+CREATE TABLE NoteLieu
+(
+    ID_Utilisateur INT,
+    ID_Lieu        INT,
+    Note           INT CHECK (Note >= 0 AND Note <= 5),
+    Commentaire    VARCHAR(255),
+    PRIMARY KEY (ID_Utilisateur, ID_Lieu),
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateur (ID_Utilisateur),
+    FOREIGN KEY (ID_Lieu) REFERENCES Lieu (ID_Lieu)
 );
