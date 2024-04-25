7a19835abddd94e1884980fba4fd10159be3ff5c
Guiza4 <youssef.guiza@yahoo.com>
2024-04-26 00:01:49 +0200

    Recherche par UserMail fonctionnelle

--- src/main/java/com/takima/backskeleton/DAO/UserDao.java
+++ src/main/java/com/takima/backskeleton/DAO/UserDao.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
+    Optional<User> findByMail(String mail);
 }
