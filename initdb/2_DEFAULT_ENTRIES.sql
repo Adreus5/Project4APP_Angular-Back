6c916d0316ec77bb4c6c5feb1c0c87e4580d20de
Guiza4 <youssef.guiza@yahoo.com>
2024-05-12 22:03:15 +0200

    Modifications des infos par défaut dans la bdd

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -1,25 +1,14 @@
 -- Insérer 5 Administrateurs avec des noms fictifs
 -- Pour les utilisateurs Admin
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Dupont', 'Jean', 'Admin', 'jean.dupont@admin.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Durand', 'Marie', 'Admin', 'marie.durand@admin.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Martin', 'Luc', 'Admin', 'luc.martin@admin.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Bernard', 'Sophie', 'Admin', 'sophie.bernard@admin.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Petit', 'Émile', 'Admin', 'emile.petit@admin.com');
-
+INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Fantasy', 'Traveler', 'Admin', 'fantasy.traveler@admin.com');
 -- Pour les clients
 INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Leroy', 'Isabelle', 'Client', 'isabelle.leroy@example.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Moreau', 'Christophe', 'Client', 'christophe.moreau@example.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Simon', 'Patricia', 'Client', 'patricia.simon@example.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Laurent', 'Olivier', 'Client', 'olivier.laurent@example.com');
-INSERT INTO Utilisateur (Nom, Prenom, Type_User, Mail) VALUES ('Lefebvre', 'Clémence', 'Client', 'clemence.lefebvre@example.com');
-
 
 -- Insérer 10 films existants en respectant les genres
-INSERT INTO Film (Nom_Film, Genre, Langue, Synopsis) VALUES ('Inception', 'Action', 'Anglais', 'Un voleur utilise la technologie pour entrer dans les rêves.');
-INSERT INTO Film (Nom_Film, Genre, Langue, Synopsis) VALUES ('Intouchables', 'Comédie', 'Français', 'L\`histoire d\une amitié entre deux hommes venant de mondes différents.');
-INSERT INTO Film (Nom_Film, Genre, Langue, Synopsis) VALUES ('Conjuring', 'Horreur', 'Anglais', 'Des enquêteurs paranormaux travaillent sur une maison hantée.');
-INSERT INTO Film (Nom_Film, Genre, Langue, Synopsis) VALUES ('Titanic', 'Drame', 'Anglais', 'Un artiste pauvre et une riche héritière tombent amoureux à bord du Titanic.');
-INSERT INTO Film (Nom_Film, Genre, Langue, Synopsis) VALUES ('Amélie', 'Romance', 'Français', 'Une serveuse décide de changer la vie des gens autour d\elle pour le mieux.');
+INSERT INTO Film (Nom_Film, Genre, Langue,realisateur, Synopsis) VALUES ('Inception', 'Action', 'Anglais','Christopher Nolan', 'Un voleur utilise la technologie pour entrer dans les rêves.');
+INSERT INTO Film (Nom_Film, Genre, Langue,realisateur, Synopsis) VALUES ('Intouchables', 'Comédie', 'Français','Olivier Nakache', 'L\`histoire d\une amitié entre deux hommes venant de mondes différents.');
+INSERT INTO Film (Nom_Film, Genre, Langue,realisateur, Synopsis) VALUES ('Conjuring', 'Horreur', 'Anglais','James Wan', 'Des enquêteurs paranormaux travaillent sur une maison hantée.');
+INSERT INTO Film (Nom_Film, Genre, Langue,realisateur, Synopsis) VALUES ('Titanic', 'Drame', 'Anglais','James Cameron', 'Un artiste pauvre et une riche héritière tombent amoureux à bord du Titanic.');
 
 INSERT INTO Lieu (Nom, Adresse, Ville, Type_Lieu, Description) VALUES ('Musée du Louvre','Rue de Rivoli, 75001 Paris', 'Paris', 'Musée', 'Le musée du Louvre, le plus grand musée d\art et d\antiquités au monde.');
 INSERT INTO Lieu (Nom, Adresse, Ville, Type_Lieu, Description) VALUES ('Bar la bastille','Place de la Bastille, 75011 Paris', 'Paris', 'Bar', 'Un bar historique au cœur de Paris.');
@@ -32,8 +21,6 @@ INSERT INTO note_film (ID_Utilisateur, ID_Film, Note, Commentaire) VALUES (1, 2,
 INSERT INTO note_film (ID_Utilisateur, ID_Film, Note, Commentaire) VALUES (2, 3, 3, 'Commentaire 7');
 INSERT INTO note_film (ID_Utilisateur, ID_Film, Note, Commentaire) VALUES (2, 4, 1, 'Commentaire 7');
 INSERT INTO note_film (ID_Utilisateur, ID_Film, Note, Commentaire) VALUES (3, 5, 2, 'Commentaire 7');
-
-
 
 INSERT INTO note_lieu (ID_Utilisateur, ID_Lieu, Note, Commentaire) VALUES (1, 2, 3, 'Commentaire 6');
 INSERT INTO note_lieu (ID_Utilisateur, ID_Lieu, Note, Commentaire) VALUES (1, 1, 2, 'Commentaire 7');
